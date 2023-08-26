93720d423bc5e8c6b11753444e0b7efcd58dd639
Gehad <gehadhisham7@gmail.com>
2023-08-27 02:20:50 +0300

    Create test_answer_questions.py

---
+++ src/tests/test_answer_questions.py
@@ -0,0 +1,28 @@
+from src.components import helper as h
+import pytest
+
+# Define test cases using the pytest
+@pytest.mark.parametrize("context, question, expected_answer", [
+    # Valid answer
+    ("France is a very beautiful country, and it has multiple cities. However, the capital of France is Paris.", "What is the capital of France?", "Paris"),
+    # No answer found
+    ("The sky is blue, the flower is red, and the car is green", "What is the color of the car?", "Green"),
+    ("      ", "What is the name of president of Italy?", "No answer found"),
+    # No relevant information
+    ("The flower is red.", "What is the color of the car?", "No answer found"),
+])
+
+# Test the answer_questions function with various scenarios.
+def test_answer_questions(context, question, expected_answer):
+    """
+    Args:
+        context: The context text.
+        question: The question.
+        expected_answer: The expected answer.
+
+    This function uses parameterized testing to check the behavior of the answer_questions function
+    in different situations.
+    """
+    predicted_answer = h.answer_questions(context, question)
+    assert predicted_answer.lower() in expected_answer.lower() 
+
